82c8ad317905b95a0514ba585586a3ac1c6fde40
VaibhavDixit0909 <vaibhavdixitcse@gmail.com>
2024-01-08 12:39:19 +0530

    footer responsiveness

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"; // Make sure to adjust this import base
 const Footer = () => {
   return (
     <footer className=" bg-slate-100 py-3  border-t-2 border-slate-200">
-      <div className="container text-xs sm:text-base px-4 md:px-0 mx-auto flex items-center justify-between">
+      <div className=" text-xs sm:text-base px-4 sm:px-10  mx-auto flex items-center justify-between">
         {/* Logo on the left */}
         <div className="flex items-center">
           <span className="sm:text-base  text-xs text-slate-500  font-bold">
